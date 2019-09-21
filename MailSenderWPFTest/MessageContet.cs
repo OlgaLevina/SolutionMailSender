9e3c3dbc9a78e9560f88163206670a6a5627509e
olga <olga.levina.s@gmail.com>
2019-09-21 15:07:51 +0300

    Выполнено дз.

---
+++ MailSenderWPFTest/MessageContet.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MailSenderWPFTest
+{
+    public class MessageContet
+    {
+        public string Body="Вы получили это письмо, потому что сами подписались на это!";
+        public string Subject = "Geekbrains - рассылка";
+        public string HTML = string.Empty;
+        public bool IsBodyHTML=false;
+        public string Attachment = string.Empty;
+    }
+}
---
+++ MailSenderWPFTest/PersonMail.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Security;
+
+namespace MailSenderWPFTest
+{
+    class PersonMail
+    {
+        public  string Login;
+        public  SecureString Password;//для хранения паролей, сторонние не смогут прочитать, но для передачи в бд не подойдет, нужно будет преобразовывать в строку
+        public string Email;
+        public string Name;
+    }
+}
