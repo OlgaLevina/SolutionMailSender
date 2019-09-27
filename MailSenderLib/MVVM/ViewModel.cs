403a9e66352b1fb2f8f0e35ef4a5c1f694a1c08f
olga <olga.levina.s@gmail.com>
2019-09-27 20:44:56 +0300

    Добавлен mvvm вручную

---
+++ MailSenderLib/MVVM/ViewModel.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace MailSenderLib.MVVM
+{
+    public abstract class ViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected virtual void OnPropertyChanged([CallerMemberName]string PropertyName=null) => //устанавливаем атрибут и значение по умолчанию, чтобы при вызове мотода без парамера, то в переменную ProperName будет автоматически передаваться имя метода, из которого был вызван данный метод, вт.ч. и имя свойства
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(PropertyName));
+
+        //упростит установку полей свойств. парамеьр  PropertyName при этом определяется автоматически
+        protected virtual bool Set<T>(ref T field, T value, [CallerMemberName]string PropertyName = null)
+        {
+            if (Equals(field, value)) return false;
+            field = value;
+            OnPropertyChanged(PropertyName);
+            return true;
+        }
+    }
+}
