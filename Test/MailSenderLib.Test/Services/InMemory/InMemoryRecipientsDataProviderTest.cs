5ee762f58926a8a8920bb4c243d8b6bb543a16a7
olga <olga.levina.s@gmail.com>
2019-10-02 14:51:08 +0300

    Добавлен тестер для реципиента

---
+++ Test/MailSenderLib.Test/Services/InMemory/InMemoryRecipientsDataProviderTest.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MailSenderLib.Services;
+using MailSenderLib.Entities;
+
+namespace MailSenderLib.Test.Services.InMemory
+{
+    /// <summary>
+    /// Сводное описание для InMemoryRecipientsDataProviderTest
+    /// </summary>
+    [TestClass]
+    public class InMemoryRecipientsDataProviderTest
+    {
+        [TestMethod]
+        public void CreateNewRecipientInEmpryProvider()
+        {
+            //1
+            var expected_recipient_id = 6;
+            var expected_recipient_name = "Recipient 6";
+            var expected_recipient_address = "recipient6@server.com";
+            var expected_recipient_description = "desc";
+            var data_provider = new InMemoryRecipientsDataProvider();
+            var new_recipient = new Recipient
+            {
+                Name = "Recipient 6",
+                Address = "recipient6@server.com",
+                Description = "desc"
+            };
+            //2
+            data_provider.Create(new_recipient);
+            var actual_id = new_recipient.Id;
+            var actual_recipient = data_provider.GetById(expected_recipient_id);
+
+            //3
+            Assert.AreEqual(expected_recipient_id, actual_id);
+            Assert.AreEqual(expected_recipient_name, actual_recipient.Name);
+            Assert.AreEqual(expected_recipient_address, actual_recipient.Address);
+            Assert.AreEqual(expected_recipient_description, actual_recipient.Description);
+        }
+
+    }
+}
