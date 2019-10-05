894f2687ac0c517cc0c9819d1f58f360d0db65ce
olga <olga.levina.s@gmail.com>
2019-10-05 15:17:32 +0300

    доделана первая задача

--- Test/MailSender.ConsoleTest/HomeWork5.cs
+++ Test/MailSender.ConsoleTest/HomeWork5.cs
@@ -15,41 +15,65 @@ namespace MailSender.ConsoleTest
 {
     internal static class HomeWork5
     {
+        public static Dates Begin { get; set; }
 
-        public static void FactorialSumStart(int number)
+        public static void FactorialSumStart(int number,Dates dates)
         {
-            Thread factorial_thread = new Thread(() => Printer(Factorial(number).ToString()));
-            factorial_thread.IsBackground = true;
-            Thread sum_thread = new Thread(() => Printer(Sum(number).ToString()));
-            sum_thread.IsBackground = true;
+            //Thread factorial_thread = new Thread(() => Printer(dates.Change(Factorial(number).ToString())));
+            //factorial_thread.IsBackground = true;
+            Begin = dates;
+            ThreadCreate(Factorial,number);
+            ThreadCreate(Sum, number);
         }
 
-        public static int Factorial(int number)
+        public static void ThreadCreate(Func<int,double> function,int number)
+        {
+            Thread thread = new Thread(() => Printer(new Dates { time = DateTime.Now }.Change(function(number).ToString())));
+            thread.Name = function.Method.Name;
+            thread.IsBackground = true;//чтобы правильно вывелось время на консоли
+            thread.Start();
+        }
+
+        public static double Factorial(int number)
         {
             if (number - 1 != 0) return number * Factorial(number - 1);
             else return number;
         }
 
-        public static int Sum(int number)
+        public static double Sum(int number)
         {
             if (number - 1 != 0) return number + Sum(number - 1);
             else return number;
         }
 
+        public static int TimeCount( Dates begin)
+        {
+
+            int result = (int)(DateTime.Now - begin.time).Milliseconds;
+            begin.time = DateTime.Now;
+            return result;
+        }
+
         private static readonly object _SyncRoot = new object();//4 байта памяти - главной сделать на чтение - и используем для группировки кода с т.зр. закрытост для прерывания разными потоками
 
-        private static void Printer(string mesage)
+        public class Dates
         {
-            ThreadTests.CheckThread();
-            for (int i = 0; i < 20; i++)
+            public string message { get; set; } = "underfined";
+            public DateTime time { get; set; }
+            public Dates Change(string msg) { message = msg; return this; }
+        }
+
+
+        private static void Printer(object dates)
+        {
+            lock (_SyncRoot)
             {
-                lock (_SyncRoot)
-                {
-                    Write($"id {Thread.CurrentThread.ManagedThreadId}");
-                    Write($" name {Thread.CurrentThread.Name=nameof(Thread.CurrentThread.Name)}: ");
-                    WriteLine($" \"{mesage}\"");
-                }
-                Thread.Sleep(100);
+                Dates Data = (Dates)dates;
+                Write($"id {Thread.CurrentThread.ManagedThreadId}");
+                Write($" name {Thread.CurrentThread.Name}: ");
+                Write($" результат: \"{Data.message}\"");
+                WriteLine($" (время выполнения - {HomeWork5.TimeCount( Data)} млс)");
+                WriteLine($"Текущее общепоточное время выполнения - {(int)(DateTime.Now - Begin.time).Milliseconds} млс");
             }
             WriteLine($"Finished  id:{Thread.CurrentThread.ManagedThreadId}");
         }
--- Test/MailSender.ConsoleTest/Program.cs
+++ Test/MailSender.ConsoleTest/Program.cs
@@ -7,6 +7,7 @@ using System.Threading;
 using static System.Console;
 using System.Globalization;
 
+
 namespace MailSender.ConsoleTest
 {
    public class Program
@@ -17,9 +18,13 @@ namespace MailSender.ConsoleTest
             //ThreadTests.Start();
             //SinchronizationTests.Start();
             //ThreadPoolTests.Start();
-            WriteLine(HomeWork5.Factorial(5));
-            WriteLine(HomeWork5.Sum(5));
+            int number = 100;
+            HomeWork5.Dates dates = new HomeWork5.Dates { time = DateTime.Now };
+            WriteLine($"Факториал {HomeWork5.Factorial(number)} - время выполнения {HomeWork5.TimeCount(dates)} млс");
+            WriteLine($"Факториал {HomeWork5.Sum(number)} - время выполнения {HomeWork5.TimeCount(dates)} млс");
+            HomeWork5.FactorialSumStart(number, dates);
             ReadLine();
+
 
         }
 
